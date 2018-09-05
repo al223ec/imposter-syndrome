edf9da350aa217d9b317d013f93097f2b9af09df
Anton Ledström <anton.ledstrom@digitalent.se>
2018-09-05 15:54:31 +0200

    curry exercise and curry implementation

--- bigO.js
+++ bigO.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // Constant runtime
 // Big O Notation: "O (1)"
 
--- curry.js
+++ curry.js
@@ -3,5 +3,33 @@ const add = x => y => x + y;
 const increment = add(1);
 const addTen = add(10);
 
+/* eslint-disable */
+
 console.log(increment(2));
 console.log(addTen(2));
+
+function curry(fn) {
+    return function() {
+        if (fn.length > arguments.length) {
+            const { slice } = Array.prototype;
+            const args = slice.apply(arguments);
+
+            return () => fn.apply(null, args.concat(slice.apply(arguments)));
+        }
+
+        return fn.apply(null, arguments);
+    };
+}
+
+const addCurry = curry((a) => a + 10);
+console.log(addCurry(10)); // 20
+
+const complex = curry((a, b, c) => a + b + c);
+console.log(complex(1, 2, 3));
+
+
+// separet arity from functions
+const get = curry((property, object) => object[property]);
+const people = [ { name: 'Anton' }, { name: 'liv' } ];
+
+console.log(people.map(get('name')));
